d8e9895e50d8bd7397053f929b0970466fc35464
codescaptain <ahmet-57-@hotmail.com>
2023-10-07 23:38:55 +0300

    part-9 [improvement] update rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
 
   def record_not_found(exception)
-    render json: {error: exception.message}.to_json, status: 404
+    render json: { error: exception.message }.to_json, status: 404
   end
 end
