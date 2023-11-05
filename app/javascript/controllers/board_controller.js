dd14df9461302fed77f9db66d226036132aa4c3e
codescaptain <ahmet-57-@hotmail.com>
2023-11-06 00:01:37 +0300

    part-16 [improvement] update board_controller.js to creating dynamic board and items

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -1,53 +1,29 @@
 import {Controller} from '@hotwired/stimulus'
-import axios from  'axios'
+import axios from 'axios'
+import {map, get, isNull} from 'lodash-es'
 
 export default class extends Controller {
-    connect() {
-        axios.get('/api/boards/1/lists')
-            .then(function (response) {
-                console.log(response.data)
-            })
 
-        const boards = [
-            {
-                "id": "board-id-1",
-                "title": "Board Title 1",
-                "class": "text-white, bg-green-500",
-                "item": [
-                    {
-                        "id": "item-id-1",
-                        "title": "Item 1",
-                        "username": "username1"
-                    },
-                    {
-                        "id": "item-id-2",
-                        "title": "Item 2",
-                        "username": "username2"
-                    }
-                ]
-            },
+    isEmpty(data) {
+        if (isNull(data)) {
+            return 'text-white, bg-red-500'
+        }
+        return data
+    }
 
-            {
-                "id": "board-id-2",
-                "title": "Board Title 2",
-                "class": "text-white, bg-red-500",
-                "item": [
-                    {
-                        "id": "item-id-1",
-                        "title": "Item 1",
-                        "username": "username1"
-                    },
-                    {
-                        "id": "item-id-2",
-                        "title": "Item 2",
-                        "username": "username2"
-                    }
-                ]
+    buildBoards(boardsData) {
+        return map(boardsData, (board) => {
+            return {
+                'id': get(board, 'id'),
+                'title': get(board, 'attributes.title'),
+                'class': this.isEmpty(get(board, 'attributes.class_list')),
+                'item': get(board, 'attributes.items')
             }
+        });
+    }
 
-        ]
-
-        var kanban = new jKanban({
+    createJkanban(boards) {
+        new jKanban({
             element: '#myKanban',
             gutter: '15px',
             widthBoard: '250px',
@@ -69,23 +45,31 @@ export default class extends Controller {
                 customHandler: "<span class='item_handle'>+</span> %title% "
             },
             click: function (el) {
-            },                             // callback when any board's item are clicked
+            },
             context: function (el, event) {
-            },                      // callback when any board's item are right clicked
+            },
             dragEl: function (el, source) {
-            },                     // callback when any board's item are dragged
+            },
             dragendEl: function (el) {
                 console.log(el)
-            },                             // callback when any board's item stop drag
+            },
             dropEl: function (el, target, source, sibling) {
-            },    // callback when any board's item drop in a board
+            },
             dragBoard: function (el, source) {
-            },                     // callback when any board stop drag
+            },
             dragendBoard: function (el) {
-            },                             // callback when any board stop drag
+            },
             buttonClick: function (el, boardId) {
-            },                     // callback when the board's button is clicked
-            propagationHandlers: [],                                         // the specified callback does not cancel the browser event. possible values: "click", "context"
+            },
+            propagationHandlers: [],
         });
+    }
+
+    connect() {
+        axios.get('/api/boards/1/lists')
+            .then((response) => {
+                const boards = this.buildBoards(response['data']['data']);
+                this.createJkanban(boards)
+            });
     }
 }
