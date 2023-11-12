fa95010b4cb2628e62db695ac9272c5487ab1a34
codescaptain <ahmet-57-@hotmail.com>
2023-11-12 13:15:25 +0300

    part-19 [feature] add ListsController#create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,13 +2,26 @@
 
 class ListsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_board, only: %i[new]
+  before_action :set_board, only: %i[new create]
 
   def new
     @list = @board.lists.new
   end
 
+  def create
+    @list = @board.lists.new(list_params)
+    if @list.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def list_params
+    params.require(:list).permit(:title, :class_list)
+  end
 
   def set_board
     @board = Board.find(params[:board_id])
