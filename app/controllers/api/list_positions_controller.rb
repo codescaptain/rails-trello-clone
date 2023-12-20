b376d0c8547fac02a3f16a9cddc6149bf27730e2
codescaptain <ahmet-57-@hotmail.com>
2023-12-20 19:32:51 +0300

    part-24 [update] fix rubocop errors

--- app/controllers/api/list_positions_controller.rb
+++ app/controllers/api/list_positions_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 module Api
   class ListPositionsController < Api::ApplicationController
-    before_action :set_board, only: %i[ update]
+    before_action :set_board, only: %i[update]
 
     def update
       @lists = @board.lists
